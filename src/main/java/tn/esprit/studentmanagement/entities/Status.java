e9967c203d43ca9061baac6addf7ebd3068d97c9
Islem MAY <IMay@somezzo.com>
2026-02-03 17:05:36 +0100

    test push

--- src/main/java/tn/esprit/studentmanagement/entities/Status.java
+++ src/main/java/tn/esprit/studentmanagement/entities/Status.java
@@ -7,5 +7,5 @@ public enum Status {
     FAILED,
     WITHDRAWN
 
-    
+
 }
--- src/main/java/tn/esprit/studentmanagement/entities/Student.java
+++ src/main/java/tn/esprit/studentmanagement/entities/Student.java
@@ -12,6 +12,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
